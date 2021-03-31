72a5e8510516423289ff7fe73be58e23744f3d77
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-03-31 19:47:11 +0300

    Project 008_GitHub added

---
+++ 008_GitHub3/008_GitHub3/source/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _008_GitHub3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World!");
+
+
+            Console.Write("Įveskite pavadinimą: ");
+            string str = Convert.ToString(Console.ReadLine());
+
+
+
+        }
+    }
+}
