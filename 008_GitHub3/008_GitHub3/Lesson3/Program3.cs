cc2f67511c6aec8f375af709910324f7dddbd61b
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-03-31 20:04:42 +0300

    Programos 3 pirmas įkėlimas
    Papildžiau dar viena klase, ir nustačiau antrą kaip main()

---
+++ 008_GitHub3/008_GitHub3/Lesson3/Program3.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_GitHub3.Lesson3
+{
+    class Program3
+    {
+        static void Main()
+        {
+            Console.WriteLine("3 3 3 ");
+        }
+
+    }
+}
