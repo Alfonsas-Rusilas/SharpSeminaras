f0fe8122b2ac97669235b208b38a5d07929c0454
Alfonsas-Rusilas <alfonsas.rusilas@gmail.com>
2021-03-31 20:53:39 +0300

    OIk

--- 2021-03-31_salygu_tikrinimas/2021-03-31_salygu_tikrinimas/Program.cs
+++ 2021-03-31_salygu_tikrinimas/2021-03-31_salygu_tikrinimas/Program.cs
@@ -6,22 +6,33 @@ namespace _2021_03_31_salygu_tikrinimas
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("==== Pradžia =====");
+            Console.Write("Įveskite pirmą skaičių : ");
+            int skaicius1 = Convert.ToInt32(Console.ReadLine());
 
-            bool isActive = false;
-            int number = 200;
+            Console.Write("Įveskite antrą skaičių : ");
+            int skaicius2 = Convert.ToInt32(Console.ReadLine());
 
+            Console.Write("Įveskite paskutinį skaičių : ");
+            int skaicius3 = Convert.ToInt32(Console.ReadLine());
 
-            if (true)
-            {
-                Console.WriteLine("true");
+            int maks = 0;
+
+            if ((skaicius1 > skaicius2) && (skaicius1 > skaicius3)) {
+                maks = skaicius1;
             }
-            else
+
+            if ((skaicius2 > skaicius1) && (skaicius2 > skaicius3))
             {
-                Console.WriteLine("cc");
+                maks = skaicius2;
             }
 
+            if ((skaicius3 > skaicius1) && (skaicius3 > skaicius2))
+            {
+                maks = skaicius3;
+            }
 
+            Console.WriteLine("Didžiausias yra skaičius: {0}", maks);
+            
 
         }
     }
